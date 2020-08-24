ea68a54b0c1bd0154bf2f744e67aa2464b232d83
mrlmx <906529775@qq.com>
2020-08-24 23:52:28 +0800

    add clean-webpack-plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "production",
+    plugins: [new CleanWebpackPlugin()],
 });
