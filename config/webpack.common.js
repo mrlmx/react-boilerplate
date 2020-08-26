a627c01f06ab174273bdabea96cece719cc0142e
mrlmx <906529775@qq.com>
2020-08-26 23:36:32 +0800

    only prod env need build with hash

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,10 +1,10 @@
 const path = require("path");
-const { ENTRY_PATH, BUILD_PATH } = require("./constant");
+const { ENTRY_PATH, BUILD_PATH, isPro } = require("./constant");
 
 module.exports = {
     entry: ENTRY_PATH,
     output: {
-        filename: "js/[name].[hash:8].js",
+        filename: isPro ? "js/[name].[hash:8].js" : "js/[name].js",
         path: BUILD_PATH,
     },
 };
