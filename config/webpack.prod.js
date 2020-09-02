88d61746b0ee5ba03d9fb70d9b995529ed20b1e0
mrlmx <906529775@qq.com>
2020-09-02 21:43:02 +0800

    update webpack source-map type

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,7 +4,8 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "production",
-    // 如果要接入错误监控平台，还是需要构建 source-map。
+    // 如果要接入错误监控平台，可以生成，但是不自动在代码中注入 source-map 地址，单独上传到代码监控平台。
+    // devtool: "nosources-source-map",
     devtool: "none",
     plugins: [new CleanWebpackPlugin()],
 });
