04eb22183b8259e37dc64f1ad3e07de4f7eac7cb
mrlmx <906529775@qq.com>
2020-09-07 23:32:27 +0800

    add url-loader & file-loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -64,6 +64,31 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 10 * 1024,
+                            name: "[name].[contenthash:8].[ext]",
+                            outputPath: "assets/images",
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot|otf)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            name: "[name].[contenthash:8].[ext]",
+                            outputPath: "assets/fonts",
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
