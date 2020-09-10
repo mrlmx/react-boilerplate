c0bfb5a13f6613b687e84421eebf6d708e0a2df4
mrlmx <906529775@qq.com>
2020-09-10 14:36:03 +0800

    add copy-webpack-plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,11 +1,18 @@
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const common = require("./webpack.common.js");
+const { PUBLIC_PATH, BUILD_PATH } = require("./constant");
 
 module.exports = merge(common, {
     mode: "production",
     // 如果要接入错误监控平台，可以生成，但是不自动在代码中注入 source-map 地址，单独上传到代码监控平台。
     // devtool: "nosources-source-map",
     devtool: "none",
-    plugins: [new CleanWebpackPlugin()],
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [{ from: PUBLIC_PATH, to: BUILD_PATH }],
+        }),
+    ],
 });
